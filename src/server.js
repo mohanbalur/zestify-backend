020caaf45008faa0f2c5414c5ab8e7fab0a20964
Thivyatharshni <thivyatharshnisozhan@gmail.com>
2026-02-20 10:39:04 +0530

    routing changes

--- src/server.js
+++ src/server.js
@@ -25,7 +25,12 @@ import { authorize } from "./middlewares/role.middleware.js";
 const app = express();
 
 // Middlewares
-const allowedOrigins = ["http://localhost:5173", "http://localhost:5174"];
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://localhost:5174",
+  "https://zestify-frontend-v2.vercel.app"
+];
+
 const corsOptions = {
   origin: (origin, callback) => {
     if (!origin || allowedOrigins.includes(origin)) {
